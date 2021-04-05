b607299e7f1d73189ad47b4aed62dfa249c2fd19
DanteMaxFlo <dante.flores@distribuidoradelaoferta.com.mx>
2021-04-05 18:17:46 -0500

    initial setup

--- src/main/java/com/maxpower/cupcakes/CupcakesMod.java
+++ src/main/java/com/maxpower/cupcakes/CupcakesMod.java
@@ -1,7 +1,9 @@
-package com.example.examplemod;
+package com.maxpower.cupcakes;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -19,13 +21,16 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod("cupcakes")
+public class CupcakesMod
 {
+
+    public static final String MOD_ID = "cupcakes";
+
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public CupcakesMod() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
@@ -48,7 +53,7 @@ public class ExampleMod
 
     private void doClientStuff(final FMLClientSetupEvent event) {
         // do something that can only be done on the client
-        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().options);
+        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
     }
 
     private void enqueueIMC(final InterModEnqueueEvent event)
